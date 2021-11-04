87f5b9a41dcea1641b51b885903094b694bc15c7
Deepayan Sarkar <deepayan.sarkar@gmail.com>
2021-11-04 10:46:45 +0530

    minor source updates

--- src/components/Banner.js
+++ src/components/Banner.js
@@ -8,8 +8,9 @@ function Banner(props) {
     <div className="Banner fadeInDown" style={{animationDelay: '0.4s'}}>
       <div className="wrapper">
         The&nbsp; <a href="https://covid19india.org">covid19india.org</a> frontend
-        using data from &nbsp; <a href="https://data.incovid19.org" 
-        >data.incovid19.org</a>. Details&nbsp; <a href="https://c19in.github.io">here</a>.
+        using data from &nbsp; <a href="https://data.covid19india.org" 
+        >data.covid19india.org</a> (till 31 October). Details&nbsp; 
+        <a href="https://c19in.github.io">here</a>.
       </div>
     </div>
   );
